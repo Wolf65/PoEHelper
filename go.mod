63e01e0d4dc5f191719c8a308832a7087cde2248
IronWolf <maltazar81@gmail.com>
2023-07-01 12:56:44 +0300

    first realization

--- go.mod
+++ go.mod
@@ -10,6 +10,8 @@ require (
 
 require (
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20221017161538-93cebf72946b // indirect
 	golang.org/x/sys v0.1.0 // indirect
 	gopkg.in/fsnotify.v1 v1.4.7 // indirect
 	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"poehelper/config"
-	"poehelper/fonts"
+	"poehelper/platform"
+	"poehelper/renderer"
 	"poehelper/window"
-	"strings"
 	"time"
 
 	imgui "github.com/AllenDang/cimgui-go"
-	"github.com/hpcloud/tail"
 )
 
 var (
@@ -26,77 +26,169 @@ var (
 // }
 
 func main() {
-	backend := imgui.CreateBackend(imgui.NewGLFWBackend())
-	backend.SetBgColor(imgui.NewVec4(0.45, 0.55, 0.6, 1.0))
-	backend.CreateWindow(config.App.Info.ProjectName, 1200, 900, 0)
-	backend.SetTargetFPS(60)
-	x, y := backend.DisplaySize()
-	config.App.Vars.DisplaySize = imgui.Vec2{X: float32(x), Y: float32(y)}
+	context := imgui.CreateContext()
 
-	imgui.CurrentIO().SetConfigFlags(imgui.CurrentIO().ConfigFlags() & ^imgui.ConfigFlagsDockingEnable)
+	defer context.Destroy()
+	io := imgui.CurrentIO()
 
-	// imgui.CurrentIO().SetIniFilename(Tststr)
+	platform, err := platform.NewGLFW(io, platform.GLFWClientAPIOpenGL3)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(-1)
+	}
+	defer platform.Dispose()
 
-	// fmt.Println(imgui.CurrentIO().IniFilename())
-	fonts.AppendDefaultFont(imgui.CurrentIO())
+	renderer, err := renderer.NewOpenGL3(io)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(-1)
+	}
+	defer renderer.Dispose()
 
-	config.App.Vars.ItemSpacing = imgui.CurrentIO().Ctx().Style().ItemSpacing()
+	Run(platform, renderer)
 
-	refreshTicket := time.NewTicker(500 * time.Millisecond)
-	refreshTicketDone := make(chan bool)
+	// backend := imgui.CreateBackend(imgui.NewGLFWBackend())
+	// backend.SetBgColor(imgui.NewVec4(0.45, 0.55, 0.6, 1.0))
+	// backend.CreateWindow(config.App.Info.ProjectName, 1200, 900, 0)
+	// backend.SetTargetFPS(60)
+	// x, y := backend.DisplaySize()
+	// config.App.Vars.DisplaySize = imgui.Vec2{X: float32(x), Y: float32(y)}
 
-	go func() {
-		for {
-			select {
-			case <-refreshTicketDone:
-				return
-			case <-refreshTicket.C:
-				imgui.GetBackend().Refresh()
-			}
-		}
-	}()
-	defer func() {
-		refreshTicket.Stop()
-		refreshTicketDone <- true
-	}()
+	// imgui.CurrentIO().SetConfigFlags(imgui.CurrentIO().ConfigFlags() & ^imgui.ConfigFlagsDockingEnable)
 
-	go func() {
-		pathLog := "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Path of Exile\\logs\\Client.txt"
-		t, err := tail.TailFile(pathLog, tail.Config{
-			ReOpen:   true,
-			Follow:   true,
-			Poll:     true,
-			Location: &tail.SeekInfo{Offset: 0, Whence: 2},
-		})
-		if err != nil {
-			fmt.Println(err)
-		}
+	// // imgui.CurrentIO().SetIniFilename(Tststr)
 
-		for {
-			select {
-			// // if the channel is done, then exit the loop
-			// case <-ctx.Done():
-			// 	t.Stop()
-			// 	t.Cleanup()
-			// 	return
-			// // get the next log line and echo it out
-			case line := <-t.Lines:
-				if line != nil {
-					// hash cffb0719 print display message
-					s := strings.Split(line.Text, " ")
-					// r, _ := regexp.Match("cffb0719", []byte(line.Text))
-					if s[4] == "cffb0719" {
-						config.TestLog = line.Text
-					}
-				}
-			}
-		}
+	// // fmt.Println(imgui.CurrentIO().IniFilename())
+	// fonts.AppendDefaultFont(imgui.CurrentIO())
 
-		// for line := range t.Lines {
-		// 	fmt.Println(line.Text)
-		// }
-	}()
-	backend.Run(loop)
+	// config.App.Vars.ItemSpacing = imgui.CurrentIO().Ctx().Style().ItemSpacing()
+
+	// refreshTicket := time.NewTicker(500 * time.Millisecond)
+	// refreshTicketDone := make(chan bool)
+
+	// go func() {
+	// 	for {
+	// 		select {
+	// 		case <-refreshTicketDone:
+	// 			return
+	// 		case <-refreshTicket.C:
+	// 			imgui.GetBackend().Refresh()
+	// 		}
+	// 	}
+	// }()
+	// defer func() {
+	// 	refreshTicket.Stop()
+	// 	refreshTicketDone <- true
+	// }()
+
+	// go func() {
+	// 	pathLog := "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Path of Exile\\logs\\Client.txt"
+	// 	t, err := tail.TailFile(pathLog, tail.Config{
+	// 		ReOpen:   true,
+	// 		Follow:   true,
+	// 		Poll:     true,
+	// 		Location: &tail.SeekInfo{Offset: 0, Whence: 2},
+	// 	})
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 	}
+
+	// 	for {
+	// 		select {
+	// 		// // if the channel is done, then exit the loop
+	// 		// case <-ctx.Done():
+	// 		// 	t.Stop()
+	// 		// 	t.Cleanup()
+	// 		// 	return
+	// 		// // get the next log line and echo it out
+	// 		case line := <-t.Lines:
+	// 			if line != nil {
+	// 				// hash cffb0719 print display message
+	// 				s := strings.Split(line.Text, " ")
+	// 				// r, _ := regexp.Match("cffb0719", []byte(line.Text))
+	// 				if s[4] == "cffb0719" {
+	// 					config.TestLog = line.Text
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+
+	// 	// for line := range t.Lines {
+	// 	// 	fmt.Println(line.Text)
+	// 	// }
+	// }()
+	// backend.Run(loop)
+}
+
+// Platform covers mouse/keyboard/gamepad inputs, cursor shape, timing, windowing.
+type Platform interface {
+	// ShouldStop is regularly called as the abort condition for the program loop.
+	ShouldStop() bool
+	// ProcessEvents is called once per render loop to dispatch any pending events.
+	ProcessEvents()
+	// DisplaySize returns the dimension of the display.
+	DisplaySize() [2]float32
+	// FramebufferSize returns the dimension of the framebuffer.
+	FramebufferSize() [2]float32
+	// NewFrame marks the begin of a render pass. It must update the cimgui IO state according to user input (mouse, keyboard, ...)
+	NewFrame()
+	// PostRender marks the completion of one render pass. Typically this causes the display buffer to be swapped.
+	PostRender()
+	// ClipboardText returns the current text of the clipboard, if available.
+	ClipboardText() (string, error)
+	// SetClipboardText sets the text as the current text of the clipboard.
+	SetClipboardText(text string)
+}
+
+type clipboard struct {
+	platform Platform
+}
+
+func (board clipboard) Text() (string, error) {
+	return board.platform.ClipboardText()
+}
+
+func (board clipboard) SetText(text string) {
+	board.platform.SetClipboardText(text)
+}
+
+// Renderer covers rendering cimgui draw data.
+type Renderer interface {
+	// PreRender causes the display buffer to be prepared for new output.
+	PreRender(clearColor [3]float32)
+	// Render draws the provided cimgui draw data.
+	Render(displaySize [2]float32, framebufferSize [2]float32, drawData imgui.DrawData)
+}
+
+const (
+	millisPerSecond = 1000
+	sleepDuration   = time.Millisecond * 25
+)
+
+func Run(p Platform, r Renderer) {
+	clearColor := [3]float32{0, 0, 0}
+	for !p.ShouldStop() {
+		p.ProcessEvents()
+
+		// Signal start of a new frame
+		p.NewFrame()
+		imgui.NewFrame()
+
+		loop()
+
+		// Rendering
+		imgui.Render() // This call only creates the draw data list. Actual rendering to framebuffer is done below.
+
+		r.PreRender(clearColor)
+		// A this point, the application could perform its own rendering...
+		// app.RenderScene()
+
+		r.Render(p.DisplaySize(), p.FramebufferSize(), imgui.CurrentDrawData())
+		p.PostRender()
+
+		// sleep to avoid 100% CPU usage for this demo
+		<-time.After(sleepDuration)
+	}
 }
 
 func loop() {
