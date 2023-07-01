ccf67a77d8bd071d462ad7d469fe157f90fb6e89
IronWolf <maltazar81@gmail.com>
2023-07-01 16:59:11 +0300

    refactoring

--- platform/glfw.go
+++ platform/glfw.go
@@ -27,11 +27,40 @@ func (err StringError) Error() string {
 type GLFWClientAPI string
 
 const (
-	// ErrUnsupportedClientAPI is used in case the API is not available by the platform.
-	ErrUnsupportedClientAPI               = StringError("unsupported ClientAPI")
-	GLFWClientAPIOpenGL3    GLFWClientAPI = "OpenGL3"
+	GLFWClientAPIOpenGL3 GLFWClientAPI = "OpenGL3"
 )
 
+// Platform covers mouse/keyboard/gamepad inputs, cursor shape, timing, windowing.
+type Platform interface {
+	// ShouldStop is regularly called as the abort condition for the program loop.
+	ShouldStop() bool
+	// ProcessEvents is called once per render loop to dispatch any pending events.
+	ProcessEvents()
+	// DisplaySize returns the dimension of the display.
+	DisplaySize() [2]float32
+	// FramebufferSize returns the dimension of the framebuffer.
+	FramebufferSize() [2]float32
+	// NewFrame marks the begin of a render pass. It must update the cimgui IO state according to user input (mouse, keyboard, ...)
+	NewFrame()
+	// PostRender marks the completion of one render pass. Typically this causes the display buffer to be swapped.
+	PostRender()
+	// ClipboardText returns the current text of the clipboard, if available.
+	ClipboardText() (string, error)
+	// SetClipboardText sets the text as the current text of the clipboard.
+	SetClipboardText(text string)
+}
+
+type clipboard struct {
+	platform Platform
+}
+
+func (board clipboard) Text() (string, error) {
+	return board.platform.ClipboardText()
+}
+
+func (board clipboard) SetText(text string) {
+	board.platform.SetClipboardText(text)
+}
 
 // GLFW implements a platform based on github.com/go-gl/glfw (v3.2).
 type GLFW struct {
